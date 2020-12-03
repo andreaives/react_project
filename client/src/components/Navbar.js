76da27909a0858e42b2a2befb53d558631853296
Travis Cook <travismcook93@gmail.com>
2020-12-03 12:17:24 -0700

    commiting before pull

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar"
 
 function Navbar() {
   return (
+    <Navbar>
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
         NAME COMING SOON
       </a>
     </nav>
+    </Navbar>
   );
 }
-
 export default Navbar;
+// for userID? just use the _id value mongo is gonna generate for every user
--- client/src/components/journals/Dream.js
+++ client/src/components/journals/Dream.js
@@ -1,1 +1,15 @@
-// Dream Journal component 
+import React from "react"
+import Card from "react-bootstrap/Card"
+
+function Journal(props){
+  return(
+    <Card>
+      {/* this is where we will recieve the data for the cards */}
+      <Card.Title>{props.title}</Card.Title>  
+      <Card.Title>{props.date}</Card.Title>
+      <Card.Text>{props.content}</Card.Text>
+    </Card>
+  )
+}
+
+export default Journal
