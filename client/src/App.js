0953172c8725c2d2af2c947c6c6e2416ffafd578
Travis Cook <travismcook93@gmail.com>
2020-12-03 11:42:22 -0700

    context is started and journals are being worked on

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {useContext, useState} from "react";
+import Navbar from "./components/Navbar"
+import UserContext from "./utils/userContext.js"
 
 function App() {
+  const [userState, setUserState] = useState({
+    userID: "",
+    userBirthday: ""
+  });
   return (
+    <>
+    <UserContext.Provider>
+    <Navbar />
+    </UserContext.Provider>
+    </>
   )
 }
 
 export default App;
+
+//is there an old activity that used it?
