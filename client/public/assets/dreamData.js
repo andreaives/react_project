69eea59d19a83ee8a101d1330b12d6840f6d57ee
MadelineC <madeline.cowell@colorado.edu>
2020-12-03 12:11:17 -0700

    Creating dummy data for Dream Journal

---
+++ client/public/assets/dreamData.js
@@ -0,0 +1,91 @@
+// dummy data for dream data
+
+export const dreamData =
+
+    [
+        {
+            title: "Mexico",
+            userId: "MadDawg",
+            content: "Last night I dreamt that I was in Cancun, Mexico! Covid has limited traveling and I miss traveling so much. I woke, started my day with a Pina Coloda and biked down to the beach. ",
+            date: "8/23/20"
+        },
+        {
+            title: "Went ice climbing",
+            userId: "MadDawg",
+            content: "Went ice climbing up a waterfall near Ouray. It was terrifying!",
+            date: "11/12/20"
+        },
+        {
+            title: "Two seasons",
+            userId: "MadDawg",
+            content: "In the beginning of the day it was summer. We went to the lake and then in the afternoon it started snowing! It was wild! ",
+            date: "11/15/20"
+        },
+        {
+            title: "Mystery Dream",
+            userId: "MadDawg",
+            content: "Last night's dream was a little fuzzy! I felt like I was running but I wasn't going anywhere.",
+            date: "11/30/20"
+        },
+        {
+            title: "Dream Job",
+            userId: "MadDawg",
+            content: "I had a dream that I owned my own coding business and made millions!",
+            date: "12/3/20"
+        },
+        {
+            title: "Today's Dream",
+            userId: "bobby1234",
+            content: "Last night I had a dream that I was riding a flying beagle through the andromeda galaxy. It was just ok, but then We went to I hop, and the waiter was like, 'NO DOGS ALLOWED'! and we had to leave.",
+            date: "12/1/2020"
+        },
+        {
+            title: "Meditation",
+            userId: "bobby1234",
+            content: "At the end of the day it is nice to reflect on the ups and downs of life. letting the mind rise and flow in vibration like the tide is pulled by the moon. Then all is one, and one is nothing...",
+            date: "12/2/2020"
+        },
+        {
+            title: "Aliens next door",
+            userId: "bobby1234",
+            content: "I swear the Jacobsons next door are not from this planet. We're going to hang out tomorrow. Looking forward to it.",
+            date: "12/3/2020"
+        },
+        {
+            title: "We're going on a trip!",
+            userId: "bobby1234",
+            content: "So... Totally went to space today with the Jacobsons. We were flying through the Andromeda Galaxy when suddenly we stopped to eat at some alien restaurant. They were so rude, and wouldn't let me come in to eat.",
+            date: "12/4/2020"
+        },
+        {
+            title: "I had a dream",
+            userId: "roger123",
+            content: "My dream was bizarre. I dreamt my cat was flying a car.",
+            date: "December 3, 2020"
+        },
+        {
+            title: "I felt a bit off today",
+            userId: "roger123",
+            content: "I didn't feel like myself today. I felt like a cat. i just wanted to sleep all day. ",
+            date: "December 2, 2020"
+        },
+        {
+            title: "Today was awesome!",
+            userId: "roger123",
+            content: "The group presentation went well and we impressed Gary!",
+            date: "December 9, 2020"
+        },
+        {
+            title: "What do I do now?",
+            userId: "roger123",
+            content: "The bootcamp is done. Now I have to find a job; I'm scared!",
+            date: "December 10, 2020"
+        },
+        {
+            title: "This Covid Christmas will be a bit depressing",
+            userId: "roger123",
+            content: "The bootcamp is done. Now I have to find a job, I'm scared!",
+            date: "December 11, 2020"
+        }
+    ]
+
--- client/src/components/journals/Dream.js
+++ client/src/components/journals/Dream.js
@@ -1,1 +1,51 @@
 // Dream Journal component 
+
+import React from "react";
+import "./App.css";
+import { dreamData } from "./data";
+
+export const DreamJournal = () => {
+    return (
+        <>
+            <div className="dream-container">
+                {dreamData.map((data, key) => {
+                    return (
+                        <div key={key}>
+                            <Employee
+                                key={key}
+                                title={data.title}
+                                userId={data.userId}
+                                content={data.content}
+                                date={data.date}
+                            />
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    );
+};
+
+const Dream = ({ title, userId, content, date }) => {
+    if (!name) return <div />
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <h5>{title}</h5>
+                    </td>
+                    <td>
+                        <h5>{userId}</h5>
+                    </td>
+                    <td>
+                        <h4>{content}</h4>
+                    </td>
+                    <td>
+                        <p>{date}</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
