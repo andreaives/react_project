13a9744b6b6ae89e08cb4ddf9688d0797793efde
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-12-03 19:01:52 -0700

    Newly updated aztro.js

--- client/src/components/selectors/Aztro.js
+++ client/src/components/selectors/Aztro.js
@@ -7,7 +7,7 @@ class Aztro extends Component {
       json: {}
     }
   }
-  
+
   fetchScope (astroSign) {
     const URL = 'https://aztro.sameerkumar.website/?sign=' + astroSign + '&day=today';
     fetch(URL, {
@@ -20,19 +20,37 @@ class Aztro extends Component {
     return (
       <>
       <div>
-        <button onClick={() => this.fetchScope("Aries")}>♈ Aries</button> <button onClick={() => this.fetchScope("Taurus")}>♉ Taurus</button> <button onClick={() => this.fetchScope("Gemini")}>♊ Gemini</button> <button onClick={() => this.fetchScope("Cancer")}>♋ Cancer</button>
-        <button onClick={() => this.fetchScope("Leo")}>♌ Leo</button> <button onClick={() => this.fetchScope("Virgo")}>♍ Virgo</button> <button onClick={() => this.fetchScope("Libra")}>♎ Libra</button> <button onClick={() => this.fetchScope("Scorpio")}>♏ Scorpio</button> 
-        <button onClick={() => this.fetchScope("Sagittarius")}>♐ Sagittarius</button> <button onClick={() => this.fetchScope("Capricorn")}>♑ Capricorn</button> <button onClick={() => this.fetchScope("Aquarius")}>♒ Aquarius</button> <button onClick={() => this.fetchScope("Pisces")}>♓ Pisces</button> 
-      </div>
-      <div>
-        Current Date: {this.state.json.current_date} <br />
-        Compatibility: {this.state.json.compatibility} <br />
-        Lucky Number: {this.state.json.lucky_number} <br />
-        Lucky Time: {this.state.json.lucky_time} <br />
-        Color: {this.state.json.color} <br />
-        Date Range: {this.state.json.date_range} <br />
-        Mood: {this.state.json.mood} <br />
-        Description: {this.state.json.description} <br />
+        <div className="jumbotron">
+          <h1 className="display-4">Daily Horoscope</h1>
+            <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+              <div className="btn-group mr-2" role="group" aria-label="Button group">
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Aries")}>♈ Aries</button>
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Taurus")}>♉ Taurus</button>
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Gemini")}>♊ Gemini</button>
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Cancer")}>♋ Cancer</button>
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Leo")}>♌ Leo</button>
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Virgo")}>♍ Virgo</button>
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Libra")}>♎ Libra</button>
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Scorpio")}>♏ Scorpio</button> 
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Sagittarius")}>♐ Sagittarius</button>
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Capricorn")}>♑ Capricorn</button>
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Aquarius")}>♒ Aquarius</button>
+                <button type="button" className="btn btn-secondary" onClick={() => this.fetchScope("Pisces")}>♓ Pisces</button> 
+              </div>
+            </div>
+          <p className="lead">Click on the button to see today's horroscope for each sign.  </p>
+          <h2>{this.astroSign}</h2>
+          <p>Birthday Between: {this.state.json.date_range} </p>
+          <p>Today's Horoscope: {this.state.json.current_date},<br/> {this.state.json.description} </p>
+        <ul>
+          <li>Extras</li>
+          <li>Compatibility: Spend some time with a {this.state.json.compatibility} today.</li>
+          <li>Lucky Number: {this.state.json.lucky_number} </li>
+          <li>Lucky Time: {this.state.json.lucky_time} </li>
+          <li>Color: {this.state.json.color} </li>
+          <li>Mood: {this.state.json.mood} </li>
+        </ul>
+        </div>
       </div>
       </>
     );
