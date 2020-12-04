6f6b77460d94a4bfe8493a02f8d3833cc7a1972b
MadelineC <madeline.cowell@colorado.edu>
2020-12-04 11:29:04 -0700

    Commitin changes

--- client/src/components/journals/Dream.js
+++ client/src/components/journals/Dream.js
@@ -1,66 +1,66 @@
-// import React from "react"
-// import Card from "react-bootstrap/Card"
+import React from "react"
+import Card from "react-bootstrap/Card"
 
-// function Journal(props){
-//   return(
-//     <Card>
-//       {/* this is where we will receive the data for the cards */}
-//       <Card.Title>{props.title}</Card.Title>  
-//       <Card.Title>{props.date}</Card.Title>
-//       <Card.Text>{props.content}</Card.Text>
-//     </Card>
-//   )
-// }
+function Journal(props){
+  return(
+    <Card>
+      {/* this is where we will receive the data for the cards */}
+      <Card.Title>{props.title}</Card.Title>  
+      <Card.Title>{props.date}</Card.Title>
+      <Card.Text>{props.content}</Card.Text>
+    </Card>
+  )
+}
 
-// export default Journal
-// // Dream Journal component im accepting both for now, im not really using the top part so it can all be changed if we need to
+export default Journal
+// Dream Journal component im accepting both for now, im not really using the top part so it can all be changed if we need to
 
-// // import React from "react";
-// import "./App.css";
-// import { dreamData } from "./dreamData";
+// import React from "react";
+import "./App.css";
+import { dreamData } from "./dreamData";
 
-// export const DreamJournal = () => {
-//     return (
-//         <>
-//             <div className="dream-container">
-//                 {dreamData.map((data, key) => {
-//                     return (
-//                         <div key={key}>
-//                             <Employee
-//                                 key={key}
-//                                 title={data.title}
-//                                 userId={data.userId}
-//                                 content={data.content}
-//                                 date={data.date}
-//                             />
-//                         </div>
-//                     )
-//                 })}
-//             </div>
-//         </>
-//     );
-// };
+export const DreamJournal = () => {
+    return (
+        <>
+            <div className="dream-container">
+                {dreamData.map((data, key) => {
+                    return (
+                        <div key={key}>
+                            <Journal
+                                key={key}
+                                title={data.title}
+                                userId={data.userId}
+                                content={data.content}
+                                date={data.date}
+                            />
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    );
+};
 
-// const Dream = ({ title, userId, content, date }) => {
-//     if (!name) return <div />
-//     return (
-//         <table>
-//             <tbody>
-//                 <tr>
-//                     <td>
-//                         <h5>{title}</h5>
-//                     </td>
-//                     <td>
-//                         <h5>{userId}</h5>
-//                     </td>
-//                     <td>
-//                         <h4>{content}</h4>
-//                     </td>
-//                     <td>
-//                         <p>{date}</p>
-//                     </td>
-//                 </tr>
-//             </tbody>
-//         </table>
-//     );
-// }
+const Dream = ({ title, userId, content, date }) => {
+    if (!name) return <div />
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <h5>{title}</h5>
+                    </td>
+                    <td>
+                        <h5>{userId}</h5>
+                    </td>
+                    <td>
+                        <h4>{content}</h4>
+                    </td>
+                    <td>
+                        <p>{date}</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
