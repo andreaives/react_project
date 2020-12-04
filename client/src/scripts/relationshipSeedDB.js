d6d623263c43aa9ad5d65d40eae9dc09b15e46e3
Brendon Hart <brendonhartbusiness@gmail.com>
2020-12-04 10:06:04 -0700

    Updated travel, job, and relationship status

---
+++ client/src/scripts/relationshipSeedDB.js
@@ -0,0 +1,17 @@
+ export const relationshipSeed = [
+    {
+        status: "Single",
+    }, {
+        status: "It's complicated",
+
+    }, {
+        status: "Married"
+    }, {
+        status: "Living with a monkey"
+    }, {
+        status: "Married to a parrot"
+    }, {
+        status: "Stuck as Gary's maid",
+        image: "./images/Screen Shot 2020-11-24 at 11.36.23 AM.png"
+    }
+]
