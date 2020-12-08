db5a553ddd0a445c56db7297f834ea117323463c
Mark Brazee <m.brazee@comcast.net>
2020-12-07 20:48:56 -0700

    what did I change

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -11,26 +11,23 @@ class Main extends Component {
       <>
         <div className="container-fluid">
           <div className="row">
-            <div className="col-sm-.5 col-md-1 col-lg-2" />
-            <div className="col-sm-11 col-md-4 col-lg-4">
+            <div className="col-sm-12 col-md-7 col-lg-7">
               <Aztro />
-              <br />
+              {/* <br />
               <h2>Readings, Links, and More.</h2>
               <br />
               <p>
                 Enjoy the links and prediction readings below to help you plan
                 your day. Use the Journal to the right to write your thoughts,
                 dreams, and feelings.
-              </p>
+              </p> */}
             </div>
-            <div className="col-sm-0 col-md-2 col-lg-2" />
-            <div className="col-sm-.5 col-md-4 col-lg-4">
+            <div className="col-sm-12 col-md-5 col-lg-5">
               <div className="scrollHeader">
-                <h1 className="display-4">Daily Journal</h1>
+                <h1 className="display-4">Journal</h1>
                 <JournalContainer />
               </div>
             </div>
-            <div className="col-sm-.5 col-md-1 col-lg-2" />
           </div>
         </div>
         <Footer />
