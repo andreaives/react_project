0d1b845b119b79cb73e9c26ef8b40b40452ff330
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-12-08 13:49:25 -0700

    new page for links

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -9,39 +9,35 @@ class Resources extends Component {
           <p style={{ color: "white" }}>
             <ul>
               <li>
-                <a href="https://astrolibrary.org/lessons/">Astro Library</a>{" "}
+                <a href="https://astrolibrary.org/lessons/" target='_blank' rel="noopener noreferrer"> Astro Library </a>
                 learn to read your own stars with this online beginners course.
-              </li>
+                </li>
               <li>
-                <a href="https://astrologybytes.podbean.com/">
-                  Astrology Bytes
-                </a>
+                <a href="https://astrologybytes.podbean.com/" target='_blank' rel="noopener noreferrer"> Astrology Bytes </a>
                 The best podcast for astrology beginners.
               </li>
               <li>
-                <a href="https://www.princeton.edu/~ezb/geomancy/geostep.html">
-                  Medieval Geomancy
-                </a>{" "}
+                <a href="https://www.princeton.edu/~ezb/geomancy/geostep.html" target='_blank' rel="noopener noreferrer"> Medieval Geomancy </a>
                 A fun and complex fortune telling system using dots on paper.
               </li>
               <li>
-                <a href="https://sigilengine.com/">Sigil Engine</a> Make magical
+                <a href="https://sigilengine.com/" target='_blank' rel="noopener noreferrer"> Sigil Engine </a> Make magical
                 works of art to represent your goals and wishes.
               </li>
               <li>
-                <a href="https://www.123test.com/personality-test/">
+                <a href="https://www.123test.com/personality-test/" target='_blank' rel="noopener noreferrer">
                   Big Five Personality Test
-                </a>{" "}
+                </a>
                 Take a scientifically validated personality test.
               </li>
               <li>
-                <a href="https://www.astrology.com/us/home.aspx">Astrology</a>{" "}
+                <a href="https://www.astrology.com/us/home.aspx" target='_blank' rel="noopener noreferrer">Astrology</a>
                 Choose your zodiac sign for a free daily horoscope.
               </li>
               <li>
-                <a href="https://www.quantumrun.com/future-timeline/2021">
+                <a href="https://www.quantumrun.com/future-timeline/2021" target='_blank' rel="noopener noreferrer" >
                   Qauntumrun
-                </a>{" "}
+                </a>
                 Don't believe in the zodiac? Take a look at some undisputeable
                 predictions for 2021.
               </li>
